c1b87593a72d979abc32c5d7882421776ba4acbb
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:11 +0100

    2017/1/1: works without circular check

---
+++ 2017/day1/haskell/main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.Char (digitToInt)
+import Data.Maybe (isJust, fromJust)
+
+adder :: Char -> Char -> Maybe Int
+adder a b = if a == b
+  then Just $ 2 * digitToInt a
+  else Nothing
+
+solve :: String -> Integer
+solve digits = sum $
+               map (toInteger.fromJust) $
+               filter isJust maybeDoubles
+  where
+    maybeDoubles :: [Maybe Int]
+    maybeDoubles = circularZip adder digits
+    circularZip :: (a -> a -> b) -> [a] -> [b]
+    circularZip f as = zipWith f as (tail as)
+
+result :: String -> Integer
+result = solve
+
+main :: IO ()
+main = putStrLn.show $ solve "112341"
