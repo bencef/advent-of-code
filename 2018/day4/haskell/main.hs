0e34ca4caab62fe6ac8a10d2b3aa2d3429f4f2e9
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:12 +0100

    read calendar

--- 2018/day4/haskell/main.hs
+++ 2018/day4/haskell/main.hs
@@ -4,7 +4,11 @@ module Main where
 
 import           Control.Applicative
 import           Data.Attoparsec.Text
+import           Data.Either
+import           Data.Foldable
 import           Data.Functor
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -33,6 +37,13 @@ data Guard
   | Unknown
   deriving (Eq, Show)
 
+data Shift = Shift Guard (Map Time GuardAction)
+  deriving (Eq, Show)
+
+type Line = (Date, GuardAction)
+
+type Calendar = Map Day Shift
+
 readDay :: Parser Day
 readDay = Day <$> decimal
               <* char '-'
@@ -59,15 +70,31 @@ readGuardAction = let readBeginShift =
                       readWakeUp = string "wakes up" $> WakeUp
                   in readBeginShift <|> readFallAsleep <|> readWakeUp
 
-readLine :: Parser (Date, GuardAction)
+readLine :: Parser Line
 readLine = (,) <$> readDate
                <* skipSpace
                <*> readGuardAction
+
+addLineToShift :: Shift -> Line -> Shift
+addLineToShift s@(Shift guard actions) (Date _ time, action) =
+  case action of
+    BeginShift id -> Shift (Guard id) actions
+    action -> let actions' = M.insert time action actions
+              in Shift guard actions'
+
+addLineToCalendar :: Calendar -> Line -> Calendar
+addLineToCalendar c l@(Date day _, _) =
+  let newShift = Shift Unknown M.empty
+      alter Nothing = Just (addLineToShift newShift l)
+      alter (Just shift) = Just (addLineToShift shift l)
+  in M.alter alter day c
 
 main :: IO ()
 main = do
   h <- openFile "../input.txt" ReadMode
   contents <- TIO.hGetContents h
   let lines = T.lines contents
-  mapM_ print (parseOnly readLine (lines !! 5))
+  let actions = rights (map (parseOnly readLine) lines)
+  let calendar = foldl' addLineToCalendar M.empty actions
+  -- print calendar
   return ()
