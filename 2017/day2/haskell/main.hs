9b8a3041ae8d0bef87e0c0d58dd367bdcc1d7b8c
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:11 +0100

    2017/2/1: finished

--- 2017/day2/haskell/main.hs
+++ 2017/day2/haskell/main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import Data.Char (isDigit)
+
+import Data.Text (Text(..))
+import qualified Data.Text as T (null, pack, split, unpack)
+
 maxDiff :: [Int] -> Int
 maxDiff [] = 0
 maxDiff (a:as) = go a a as
@@ -8,5 +13,13 @@ maxDiff (a:as) = go a a as
     go minV maxV [] = maxV - minV
     go minV maxV (a:as) = go (min minV a) (max maxV a) as
 
+readRows :: Text -> [[Int]]
+readRows = map (collectInts.(T.split (=='\t'))) . getRows
+  where
+    collectInts :: [Text] -> [Int]
+    collectInts = map read . filter (all isDigit) . map T.unpack
+    getRows :: Text -> [Text]
+    getRows = filter (not.T.null) . T.split (=='\n')
+
 main :: IO ()
-main = putStrLn.show $ maxDiff [2, 3, 7, 10]
+main = putStrLn.show.sum.(map maxDiff).readRows.T.pack =<< readFile "../input"
