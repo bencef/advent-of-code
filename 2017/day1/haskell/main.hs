540ceb9b88c66a21af32da5e625be90ad0e8e47e
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:11 +0100

    2017/1/2: works whooo

--- 2017/day1/haskell/main.hs
+++ 2017/day1/haskell/main.hs
@@ -3,23 +3,30 @@ module Main where
 import Data.Char (digitToInt, isDigit)
 import Data.Maybe (isJust, fromJust)
 
+data Part = Part1 | Part2
+
 adder :: Char -> Char -> Maybe Int
 adder a b = if a == b
   then Just $ digitToInt a
   else Nothing
 
-solve :: String -> Integer
-solve digits = sum $
-               map (toInteger.fromJust) $
-               filter isJust duplicates
+solve :: Part -> String -> Integer
+solve part digits = case part of
+  Part1 -> go 1 digits
+  Part2 -> go ((length digits) `div` 2) digits
   where
-    duplicates :: [Maybe Int]
-    duplicates = circularZip adder digits
-    circularZip :: (a -> a -> b) -> [a] -> [b]
-    circularZip f as = zipWith f as (drop 1 (cycle as))
+    go :: Int -> String -> Integer
+    go offset digits = sum $
+                       map (toInteger.fromJust) $
+                       filter isJust duplicates
+      where
+        duplicates :: [Maybe Int]
+        duplicates = circularZip adder digits
+        circularZip :: (a -> a -> b) -> [a] -> [b]
+        circularZip f as = zipWith f as (drop offset (cycle as))
 
 main :: IO ()
-main = readFile input >>= putStrLn.show.solve.(filter isDigit)
+main = readFile input >>= putStrLn.show.(solve Part2).(filter isDigit)
   where
     input :: String
     input = "../input"
