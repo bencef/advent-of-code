bf7e18e292c0f5c613584606769773340caf9ab1
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:12 +0100

    2018/4 parsing

--- 2018/day4/haskell/main.hs
+++ 2018/day4/haskell/main.hs
@@ -1,9 +1,38 @@
 module Main where
 
+import           Data.Attoparsec.Text
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import           System.IO
+
+data Day = Day { dayYear  :: Int
+               , dayMonth :: Int
+               , dayDay   :: Int }
+           deriving (Eq, Show, Ord)
+
+data Time = Time { timeHours   :: Int
+                 , timeMinutes :: Int }
+            deriving (Eq, Show, Ord)
+
+data Date = Date Day Time
+            deriving (Eq, Show, Ord)
+
+readDay :: Parser Day
+readDay = Day <$> decimal
+              <* char '-'
+              <*> decimal
+              <* char '-'
+              <*> decimal
+
+readTime :: Parser Time
+readTime = Time <$> decimal
+                <* char ':'
+                <*> decimal
+
+readDate :: Parser Date
+readDate = Date <$> (char '[' *> readDay)
+                <*> (skipSpace *> readTime <* char ']')
 
 main :: IO ()
 main = do
