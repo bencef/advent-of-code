150ec84734a122545c4be285bea4b4983ca70302
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:12 +0100

    day 1 part 2

--- 2022/01/part2/main.ml
+++ 2022/01/part2/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+open Core
+open Twentytwoday1
+
+module Array = ArrayLabels
+
+let () =
+  let elves = Reading.from_file "input" in
+  let number_of_elves = elves |> Array.length in
+  let calories = elves |> Array.map ~f:(Elf.get_all_calories) in
+  calories |> Array.sort ~cmp:(Int.compare);
+  let top_three = Array.sub calories ~pos:(number_of_elves-3) ~len:3 in
+  let sum = top_three |> Array.fold_left ~f:(+) ~init:0 in
+  print_endline (sum |> Int.to_string)
