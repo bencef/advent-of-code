31d5851495783da762938125dbb2d8e55d7d6b29
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:14 +0100

    add unit tests

--- 2022/07/lib/domain.ml
+++ 2022/07/lib/domain.ml
@@ -8,6 +8,39 @@ module Fs = struct
   type dir_entry = Cd_Root | Cd_Up | Cd_Dir of string
   type command_invokation = Ls of entry list | Cd of dir_entry
 
-  let initial_fs = Dir_Node {name = "/"; nodes=[||]}
+  let initial_fs = Dir_Node { name = "/"; nodes = [||] }
+
+  module Zipper = struct
+    let from_fs = Fun.id
+    let to_fs = Fun.id
+  end
+
   let from_commands _commmands = initial_fs
+
+  let equal fs_1 fs_2 =
+    match (fs_1, fs_2) with
+    | File_Node _data1, File_Node _data2 -> false
+    | Dir_Node _data1, Dir_Node _data2 -> false
+    | _ -> false
+
+  let to_string = function
+    | File_Node data -> Printf.sprintf "File: %d %s" data.size data.name
+    | Dir_Node data ->
+        Printf.sprintf "Dir: %s files: %d" data.name (data.nodes |> Array.length)
+end
+
+module Tests = struct
+  open Fs
+
+  let assert_equal actual ~expected =
+    if equal actual expected then true
+    else (
+      Printf.printf "Expected: %s\nGot: %s\n" (to_string expected)
+        (to_string actual);
+      false)
+
+  let%test "initial FS can be zipped back up" =
+    let z = Zipper.from_fs initial_fs in
+    let fs = Zipper.to_fs z in
+    assert_equal fs ~expected:initial_fs
 end
