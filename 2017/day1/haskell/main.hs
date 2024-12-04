95a402ffe70b4c72893019183bec828653a593d8
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:11 +0100

    2017/1/1: works

--- 2017/day1/haskell/main.hs
+++ 2017/day1/haskell/main.hs
@@ -1,25 +1,25 @@
 module Main where
 
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isDigit)
 import Data.Maybe (isJust, fromJust)
 
 adder :: Char -> Char -> Maybe Int
 adder a b = if a == b
-  then Just $ 2 * digitToInt a
+  then Just $ digitToInt a
   else Nothing
 
 solve :: String -> Integer
 solve digits = sum $
                map (toInteger.fromJust) $
-               filter isJust maybeDoubles
+               filter isJust duplicates
   where
-    maybeDoubles :: [Maybe Int]
-    maybeDoubles = circularZip adder digits
+    duplicates :: [Maybe Int]
+    duplicates = circularZip adder digits
     circularZip :: (a -> a -> b) -> [a] -> [b]
-    circularZip f as = zipWith f as (tail as)
+    circularZip f as = zipWith f as (tail as ++ as)
 
 main :: IO ()
-main = readFile input >>= putStrLn.show.solve
+main = readFile input >>= putStrLn.show.solve.(filter isDigit)
   where
     input :: String
     input = "../input"
