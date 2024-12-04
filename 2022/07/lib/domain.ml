31e1d5ef62ccba3fe4e07e2ad69b6ac6cd3a38b6
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:14 +0100

    make FS type non-abstract

--- 2022/07/lib/domain.ml
+++ 2022/07/lib/domain.ml
@@ -1,10 +1,13 @@
 module Fs = struct
-  type t = unit
-  type entry = Dir of string | File of { name : string; size : int }
-  type dir_entry = | Cd_Root | Cd_Up | Cd_Dir of string
-  type command_invokation =
-    | Ls of entry list
-    | Cd of dir_entry
+  type file_data = { name : string; size : int }
 
-  let from_commands _commmands = ()
+  type dir_data = { name : string; nodes : t array }
+  and t = Dir_Node of dir_data | File_Node of file_data
+
+  type entry = Dir of string | File of file_data
+  type dir_entry = Cd_Root | Cd_Up | Cd_Dir of string
+  type command_invokation = Ls of entry list | Cd of dir_entry
+
+  let initial_fs = Dir_Node {name = "/"; nodes=[||]}
+  let from_commands _commmands = initial_fs
 end
--- 2022/07/lib/domain.mli
+++ 2022/07/lib/domain.mli
@@ -1,10 +1,12 @@
 module Fs : sig
-  type t
-  type entry = Dir of string | File of { name : string; size : int }
-  type dir_entry = | Cd_Root | Cd_Up | Cd_Dir of string
-  type command_invokation =
-    | Ls of entry list
-    | Cd of dir_entry
+  type file_data = { name : string; size : int }
+
+  type dir_data = { name : string; nodes : t array }
+  and t = Dir_Node of dir_data | File_Node of file_data
+
+  type entry = Dir of string | File of file_data
+  type dir_entry = Cd_Root | Cd_Up | Cd_Dir of string
+  type command_invokation = Ls of entry list | Cd of dir_entry
 
   val from_commands : command_invokation list -> t
 end
