0a7a8c56cc9926bcda4d7a6e81956431f676d64a
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:12 +0100

    2018 day 1

---
+++ 2018/day1/haskell/main.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import           Control.Monad
+import           Data.Maybe
+import qualified Data.Set as Set
+import           Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+import           System.IO
+
+readNum :: Text -> Maybe Integer
+readNum t = case T.uncons t of
+  Nothing -> Nothing
+  Just (sign, number) ->
+    let num :: Integer
+        num = read (T.unpack number)
+    in case sign of
+         '-' -> Just (-1 * num)
+         '+' -> Just num
+         _ -> Nothing
+
+findDuplicate :: [Integer] -> Maybe Integer
+findDuplicate nums = go (Set.singleton 0) 0 nums
+  where
+    go _ _ [] = Nothing
+    go soFar last (next:rest) =
+      let new = next + last
+      in if Set.member new soFar
+         then Just new
+         else go (Set.insert new soFar) new rest
+
+main :: IO ()
+main = do
+  h <- openFile "../input.txt" ReadMode
+  contents <- TIO.hGetContents h
+  let lines = T.lines contents
+  let nums = catMaybes (map readNum lines)
+  mapM_ (putStrLn . show) (findDuplicate (cycle nums))
