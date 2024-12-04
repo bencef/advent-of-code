a31d52d9aeb1c24238696172c2a279148cb9a697
Bence Fabian <begnoc@gmail.com>
2024-12-04 13:08:15 +0100

    moar tests

--- 2024/04/lib/word_puzzle.ml
+++ 2024/04/lib/word_puzzle.ml
@@ -11,7 +11,37 @@ let find_indices t letter =
       Array.filter_mapi col ~f:(fun col l ->
           if phys_equal l letter then Some { row; col } else None))
 
-let star_indices _t _index = ()
+let star_indices t index =
+  let row_num = Array.length t in
+  let col_num = if row_num = 0 then 0 else Array.length t.(0) in
+  let inside_puzzle { row; col } =
+    let valid actual ~max_val = Int.(actual >= 0 && actual < max_val) in
+    valid row ~max_val:row_num && valid col ~max_val:col_num
+  in
+  if not (inside_puzzle index) then [||]
+  else
+    let steps =
+      let modify row_f col_f { row; col } =
+        { row = row_f row; col = col_f col }
+      in
+      let inc x = x + 1 in
+      let dec x = x - 1 in
+      let stay x = x in
+      let dirs = [ stay; inc; dec ] in
+      (* this generates a stay/stay, but that should be fine, just drop the head *)
+      List.concat_map dirs ~f:(fun row_dir ->
+          List.map dirs ~f:(fun col_dir -> modify row_dir col_dir))
+      |> fun l -> List.drop l 1
+    in
+    List.filter_map steps ~f:(fun step ->
+        let open Sequence in
+        let res =
+          unfold ~f:(fun last -> Some (last, step last)) ~init:index
+          |> Fun.flip take 4 |> filter ~f:inside_puzzle |> to_array
+        in
+        if Array.length res = 4 then Some res else None)
+    |> Array.of_list
+
 let at t { row; col } = t.(row).(col)
 
 module Tests = struct
@@ -34,6 +64,7 @@ module Tests = struct
   end
 
   module IndexArray = ArrayOf (Index)
+  module IndexArrayArray = ArrayOf (IndexArray)
 
   let%test "letter equals" = assert_equal (module Letter) X ~expected:X
 
@@ -46,4 +77,87 @@ module Tests = struct
     let puzzle = from_lists [] in
     let actual = find_indices puzzle X in
     assert_equal (module IndexArray) actual ~expected:[||]
+
+  let%test "empty puzzle has no star formation at 0,0" =
+    let puzzle = from_lists [] in
+    let actual = star_indices puzzle { row = 0; col = 0 } in
+    assert_equal (module IndexArrayArray) actual ~expected:[||]
+
+  let%test "single xmas has one star formation" =
+    let puzzle = from_lists [ [ X; X; X; X ] ] in
+    let actual = star_indices puzzle { row = 0; col = 0 } in
+    let expected =
+      [|
+        [|
+          { row = 0; col = 0 };
+          { row = 0; col = 1 };
+          { row = 0; col = 2 };
+          { row = 0; col = 3 };
+        |];
+      |]
+    in
+    assert_equal (module IndexArrayArray) actual ~expected
+
+  let%test "two lines have one star formation" =
+    let puzzle = from_lists [ [ X; X; X; X ]; [ X; X; X; X ] ] in
+    let actual = star_indices puzzle { row = 0; col = 0 } in
+    let expected =
+      [|
+        [|
+          { row = 0; col = 0 };
+          { row = 0; col = 1 };
+          { row = 0; col = 2 };
+          { row = 0; col = 3 };
+        |];
+      |]
+    in
+    assert_equal (module IndexArrayArray) actual ~expected
+
+  let%test "four lines have 3 star formation" =
+    let puzzle =
+      from_lists
+        [ [ X; X; X; X ]; [ X; X; X; X ]; [ X; X; X; X ]; [ X; X; X; X ] ]
+    in
+    let actual = star_indices puzzle { row = 0; col = 0 } in
+    let expected =
+      [|
+        (* stay inc *)
+        [|
+          { row = 0; col = 0 };
+          { row = 0; col = 1 };
+          { row = 0; col = 2 };
+          { row = 0; col = 3 };
+        |];
+        (* inc stay *)
+        [|
+          { row = 0; col = 0 };
+          { row = 1; col = 0 };
+          { row = 2; col = 0 };
+          { row = 3; col = 0 };
+        |];
+        (* inc inc *)
+        [|
+          { row = 0; col = 0 };
+          { row = 1; col = 1 };
+          { row = 2; col = 2 };
+          { row = 3; col = 3 };
+        |];
+      |]
+    in
+    assert_equal (module IndexArrayArray) actual ~expected
+
+  let%test "single xmas has one star formation from the back" =
+    let puzzle = from_lists [ [ X; X; X; X ] ] in
+    let actual = star_indices puzzle { row = 0; col = 3 } in
+    let expected =
+      [|
+        [|
+          { row = 0; col = 3 };
+          { row = 0; col = 2 };
+          { row = 0; col = 1 };
+          { row = 0; col = 0 };
+        |];
+      |]
+    in
+    assert_equal (module IndexArrayArray) actual ~expected
 end
